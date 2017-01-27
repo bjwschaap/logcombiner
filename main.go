e00d013096266b38cf8121dbc7fdf33ef690a920
Bastiaan Schaap <bastiaan.schaap@gmail.com>
2017-01-28 00:04:15 +0100

    First working draft

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"strings"
+	"regexp"
+
+	"github.com/oleiade/lane"
 )
 
 // LogLine defines a single json log event
@@ -16,12 +19,15 @@ type LogLine struct {
 	Stream string `json:"stream"`
 }
 
+var timeStampRegex = regexp.MustCompile(`^\[\d{4}-\d{1,2}-\d{1,2}.*\]`)
+
 func main() {
 	logFile, err := os.Open("./docker_log_json_stacktrace.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(logFile)
+	buffer := lane.NewQueue()
 	for scanner.Scan() {
 		line := scanner.Text()
 		var logLine LogLine
@@ -29,6 +35,39 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(strings.TrimRight(logLine.Log, "\n"))
+		if isPrimaryLine(logLine.Log) && buffer.Size() > 0 {
+			// flush out buffer to single new event
+			newLogEvent, err := json.Marshal(flush(buffer))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(newLogEvent))
+		}
+		buffer.Enqueue(logLine)
 	}
+	if buffer.Size() > 0 {
+		newLogEvent, err := json.Marshal(flush(buffer))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(newLogEvent))
+	}
+}
+
+func isPrimaryLine(line string) bool {
+	if timeStampRegex.FindString(line) != "" {
+		return true
+	}
+	return false
+}
+
+func flush(buffer *lane.Queue) *LogLine {
+	var stringBuffer bytes.Buffer
+	time := buffer.Head().(LogLine).Time
+	stream := buffer.Head().(LogLine).Stream
+	for buffer.Head() != nil {
+		logLine := buffer.Dequeue().(LogLine)
+		stringBuffer.WriteString(logLine.Log)
+	}
+	return &LogLine{Time: time, Stream: stream, Log: stringBuffer.String()}
 }
